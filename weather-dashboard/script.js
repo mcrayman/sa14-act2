82d9205347e96463a715463d81ce5fbe705c10ad
Matt McMeans <mrmcmans@memphis.edu>
2024-04-04 13:33:45 -0500

    weather icons

--- weather-dashboard/script.js
+++ weather-dashboard/script.js
@@ -16,9 +16,13 @@ async function getWeather(city) {
     document.getElementById('city-name').textContent = data.location.name;
     document.getElementById('city-region').textContent = data.location.region;
     document.getElementById('city-country').textContent = data.location.country;
-    document.getElementById('time').textContent = data.location.localtime;
+    const date = new Date(data.location.localtime);
+    const day = date.toLocaleDateString('en-US', { weekday: 'long' });
+    document.getElementById('time').textContent = day;
+    const time = date.toLocaleTimeString('en-US', { timeStyle: 'short' });
+    document.getElementById('time').textContent += ' ' + time;
     document.getElementById('temperature').textContent = data.current.temp_f + '°F';
-    document.getElementById('weather-description').textContent = data.current.condition.text;
+    document.getElementById('weather-description').src = '../' + data.current.condition.icon.replace('//cdn.weatherapi.com/', '');
     document.getElementById('humidity').textContent = 'Humidity: ' + data.current.humidity + '%';
 
     // Update HTML elements with the received data for the next 5 days
@@ -26,10 +30,14 @@ async function getWeather(city) {
     for (let i = 0; i < forecast.length; i++) {
       const day = forecast[i];
       const dayElement = document.getElementById(`day-${i}`);
-      dayElement.querySelector('.date').textContent = day.date;
+      const date = new Date(day.date);
+      const dayOfWeek = date.toLocaleDateString('en-US', { weekday: 'long' });
+      const month = date.toLocaleDateString('en-US', { month: 'long' });
+      const dayOfMonth = date.toLocaleDateString('en-US', { day: 'numeric' });
+      dayElement.querySelector('.date').textContent = `${dayOfWeek}, ${month} ${dayOfMonth}`;
       dayElement.querySelector('.max-temp').textContent = 'High ' + day.day.maxtemp_f + '°F';
       dayElement.querySelector('.low-temp').textContent = 'Low ' + day.day.mintemp_f + '°F';
-      dayElement.querySelector('.weather-description').textContent = day.day.condition.text;
+      dayElement.querySelector('#weather-description').src = '../' + day.day.condition.icon.replace('//cdn.weatherapi.com/', '');
     }
     
   } catch (error) {
